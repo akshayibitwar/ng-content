4ff8babd42aa51e452e1630e29702c67c9a87dbb
akshayibitwar <akshayebitwar@gmail.com>
2023-10-27 23:44:26 +0530

    ng-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TestContentComponent } from './shared/components/test-content/test-content.component';
+import { AuthComponentComponent } from './shared/components/auth/auth-component/auth-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestContentComponent,
+    AuthComponentComponent
   ],
   imports: [
     BrowserModule
